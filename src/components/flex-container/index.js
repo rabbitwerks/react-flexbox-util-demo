2a26232d994e6a99dc4ea1b3e3b82f1ecdf21adb
SEAN_RABBITWERKS <rabbitwerks.js@gmail.com>
2020-02-05 23:34:46 -0600

    add: cleaned up starter code, set up new files and layout, initial commit :sparkles:

---
+++ src/components/flex-container/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function FlexContainer() {
+  return(
+    <div className="flex-container fxbx spc-ctr">
+      <span>
+        Flex Container
+      </span>
+    </div>
+  )
+}
+
+export default FlexContainer;
