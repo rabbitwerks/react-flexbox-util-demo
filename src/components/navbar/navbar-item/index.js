9ea5a82b487a2d7b0b1bcd433ed098abddbed1f2
SEAN_RABBITWERKS <rabbitwerks.js@gmail.com>
2020-02-06 22:03:31 -0600

    added: navItem cmp

---
+++ src/components/navbar/navbar-item/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const styles = {
+  navItem: {
+    color: '#ccc',
+    margin: '.5rem 0',
+    borderBottom: '2px solid #ccc',
+    textDecoration: 'none',
+  }
+}
+
+function NavItem({text, targetLocation}) {
+  return (
+    <a href={targetLocation} style={styles.navItem}>
+      {text}
+    </a>
+  )
+}
+
+export default NavItem;
--- src/components/navbar/navbar-main/index.js
+++ src/components/navbar/navbar-main/index.js
@@ -1,23 +1,18 @@
 import React from 'react';
+import NavItem from '../navbar-item';
 
 const styles = {
   navbarMain: {
-    'margin-right': '2rem',
+    marginRight: '2rem',
   }
 }
 
 function NavbarMain() {
   return (
     <nav className="navbar--main fxbx fd-c" style={styles.navbarMain}>
-      <span className="flex-item">
-        Menu Item 1
-      </span>
-      <span className="flex-item">
-        Menu Item 2
-      </span>
-      <span className="flex-item">
-        Menu Item 3
-      </span>
+      <NavItem text="Home" targetLocation="http://localhost:3000/" />
+      <NavItem text="Playground" targetLocation="http://localhost:3000/playground" />
+      <NavItem text="Support" targetLocation="http://localhost:3000/support" />
     </nav>
   )
 }
